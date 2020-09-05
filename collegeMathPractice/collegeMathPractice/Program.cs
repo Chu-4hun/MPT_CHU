ba4489435d26b1b918b9b3d5dd0a5cf12030288e
Dan Hludencov <70685495+Chu-4hun@users.noreply.github.com>
2020-09-05 08:56:16 +0300

    MPT-Pract1

---
+++ collegeMathPractice/collegeMathPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace collegeMathPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Double c = Convert.ToDouble(Console.ReadLine());
+            Double d = Convert.ToDouble(Console.ReadLine());
+            Double a = Convert.ToDouble(Console.ReadLine());
+
+            Double res = ((Math.Atan(c / 4) - d * 62) / (a*a-1));
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
