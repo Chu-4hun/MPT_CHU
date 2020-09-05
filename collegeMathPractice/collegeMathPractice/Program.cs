daf11bc7428b1ec6fb3012dbd96adc7ced65dbc5
Dan Hludencov <70685495+Chu-4hun@users.noreply.github.com>
2020-09-05 09:31:14 +0300

    Update Program.cs

--- collegeMathPractice/collegeMathPractice/Program.cs
+++ collegeMathPractice/collegeMathPractice/Program.cs
@@ -10,8 +10,9 @@ namespace collegeMathPractice
             Double c = Convert.ToDouble(Console.ReadLine());
             Double d = Convert.ToDouble(Console.ReadLine());
             Double a = Convert.ToDouble(Console.ReadLine());
-
+            //task 10
             Double res1 = ((4*c+d-1)/(c-Math.Tan(a/2)));
+            //task 4
             Double res2 = ((Math.Log10(2 * c) + d - 52)/((a/4)+1));
             Console.WriteLine(res1);
             Console.WriteLine(res2);
